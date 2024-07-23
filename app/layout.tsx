59c51f21975c06dbdab57f3c39600bc7744098bc
Shashank RM <ugs22047_ece.shashank@cbit.org.in>
2024-07-23 14:04:46 +0530

    added functionalities

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,9 @@ import { Inter as FontSans } from "next/font/google"
 import { cn } from "@/lib/utils"
 import './globals.css'
 import { Metadata } from "next"
-
+import { ClerkProvider } from "@clerk/nextjs"
+import { dark } from "@clerk/themes"
+import Provider from "./Provider"
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -17,8 +19,15 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    
-      
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+        variables: { 
+          colorPrimary: "#3371FF" ,
+          fontSize: '16px'
+        },
+      }}
+    >
       <html lang="en" suppressHydrationWarning>
         <body
           className={cn(
@@ -26,11 +35,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             fontSans.variable
           )}
         >
-          
+          <Provider>
             {children}
-          
+          </Provider>
         </body>
       </html>
-    
+    </ClerkProvider>
   )
 }
